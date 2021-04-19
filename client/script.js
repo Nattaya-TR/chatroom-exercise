35003c13dca3e48b04cc562b96028ebb26082c86
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-04-19 17:55:22 +0200

    must have feature

--- client/script.js
+++ client/script.js
@@ -1,16 +1,27 @@
 let socket = io.connect();
 let target = document.getElementById('target');
 
+let username = prompt("Create your username");
+socket.emit('addNewUser', (username));
+
+console.log(username);
+
 document.getElementById('sendToAll').addEventListener('click', function () {
     let message = document.getElementById("message").value;
-    socket.emit('sendToAll',(message));
+    socket.emit('sendToAll', (message));
 })
 
 document.getElementById('sendToMe').addEventListener('click', function () {
     let message = document.getElementById("message").value;
-    socket.emit('sendToMe',(message));
+    socket.emit('sendToMe', (message));
 })
 
-socket.on('displayMessage',(message) => {
-    target.innerHTML += '<br>'+message;
+socket.on('logIn', (username) => {
+    target.innerHTML += '<br>' + username + "  has joined the chatroom ";
+})
+
+socket.on('displayMessage', ({username,message}) => {
+    target.innerHTML += '<br>' + username + ' : ' + message;
 });
+
+
--- server/server.js
+++ server/server.js
@@ -8,21 +8,46 @@ const io = require('socket.io')(server);
 const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
 
-//let counter = 0;
+let users = 1;
 
 server.listen(9004, () => {
     console.log("server running on " +9004);
 });
 
 io.on('connection', (socket) => {
+    let addedUser = false;
     console.log(' someone connected');
+
+    //Client emits 'create username", this listens and executes
+    socket.on('addNewUser', (username) => {
+        if (addedUser) return;
+
+        //store the username in the socket session for this client
+        socket.username = username;
+        users++;
+        addedUser = true;
+        io.emit('logIn', (username));
+    });
+        /*echo (all client) that has connected
+        socket.broadcast.emit('user joined',{
+            username : socket.username,
+            users : users
+        });*/
+
     socket.on('sendToAll', (message) => {
-        io.emit("displayMessage", (message));
+        io.emit("displayMessage", {
+            username : socket.username,
+            message : message
+        });
     });
 
     socket.on('sendToMe', (message) => {
-        socket.emit("displayMessage", (message));
+        socket.emit("displayMessage", {
+            username : socket.username,
+            message : message
+        });
     });
+
 });
 
 
